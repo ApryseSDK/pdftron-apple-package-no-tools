debeef53a278874f6d9ffac863717fd43894e094
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2022-02-11 15:50:05 -0800

    Update for PDFTron iOS 9.2.079330

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PDFNet",
-            url: "https://www.pdftron.com/downloads/ios/packages/9.1.2-78728/PDFNet.xcframework.zip",
-            checksum: "b2f1da88e3c0a313e88f5a74649b57c3db6af2f58ac6536585c3cc8d52a4d77c"),
+            url: "https://www.pdftron.com/downloads/ios/packages/9.2.0-79330/PDFNet.xcframework.zip",
+            checksum: "6cfd628676b633127f11e25cecdcf82c85a586b82051bb164c34d118d6c40bee"),
     ]
 )
