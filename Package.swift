d96f967640610aebfe1e28e5ee581ce91f56409c
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2023-12-13 14:42:57 -0700

    Update checksum for PDFNet framework

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
         .binaryTarget(
             name: "PDFNet",
             url: "https://www.pdftron.com/downloads/ios/packages/10.6.0-83126/PDFNet.xcframework.zip",
-            checksum: "b3bece6e44484f578fd1a5e92599a373979186810410d88715c7c4e3b6553564"),
+            checksum: "1474b504098a43cc469415e8244868ba533efe3ba790987e3ff72562862b2d35"),
     ]
 )
